a4b91b93ae210546aa6ea338b0c4e8d5b3f23ecd
Juanfer <jfvilladiego@vcb.com.co>
2019-10-15 13:07:23 -0500

    Save file

---
+++ app/Http/Controllers/PhotoController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\photo;
+use Illuminate\Http\Request;
+use Validator,Redirect,Response,File;
+//use Image;
+
+class PhotoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Request $request)
+    {
+
+        request()->validate([
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+        /* */
+
+        $files = $request->file('image');
+
+        if ( !empty($files) ) {
+            $destinationPath = 'images/'; // upload path
+            $profileImage = date('YmdHis') . "." . $files->getClientOriginalExtension();
+            $files->move($destinationPath, $profileImage);
+        }
+
+        return response()->json([
+            'status' => 'ok',
+        ]);
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\photo  $photo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(photo $photo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\photo  $photo
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(photo $photo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\photo  $photo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, photo $photo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\photo  $photo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(photo $photo)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,9 @@ use Illuminate\Http\Request;
 Route::prefix('v1')->group(function(){
     Route::post('login', 'Api\AuthController@login');
     Route::post('register', 'Api\AuthController@register');
+
+    Route::post('photos', 'PhotoController@create');
+
     Route::group(['middleware' => 'auth:api'], function(){
         Route::post('getUser', 'Api\AuthController@getUser');
     });
